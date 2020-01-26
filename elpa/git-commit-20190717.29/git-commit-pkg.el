e0e69ccd3f95673f9fa99ac8c9b1925dfd945308
Hongxin Liang <honnix@spotify.com>
2020-01-26 22:16:29 +0100

    upgrade

--- elpa/git-commit-20190717.29/git-commit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-commit" "20190717.29" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (with-editor "20181103")) :commit "bcd161d8ad3fcd80cbf69e7720c1d75a79415021" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
--- elpa/plantuml-mode-20190822.1403/plantuml-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "plantuml-mode" "20190822.1403" "Major mode for PlantUML" '((dash "2.0.0") (emacs "25.0")) :commit "2f8170b30b9885b5a6992047662d5ba0f4ac2248" :keywords '("uml" "plantuml" "ascii") :authors '(("Zhang Weize (zwz)")) :maintainer '("Carlo Sciolla (skuro)"))
