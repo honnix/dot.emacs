fc77e5017271ceceeb4a7f66063713f41a129490
Hongxin Liang <honnix@spotify.com>
2020-02-07 14:45:05 +0100

    clean up go

--- personal/27go.el
+++ personal/27go.el
@@ -1,18 +1,18 @@
 (use-package go-mode
   :ensure t
   :mode "\\.go\\'"
-  :hook (go-mode . my-go-mode-hook)
-  
+  :init
+  (setq go-guru-scope ".")
+  (setq-default indent-tabs-mode 1)
+  (setq-default tab-width 4)
+  :bind (("C-c C-f c" . go-remove-unused-imports)
+         ("M-." . godef-jump)
+         ("M-*" . pop-tag-mark))
+  :hook ((go-mode . my-go-mode-hook)
+		 (before-save . gofmt-before-save))
   :config
-  (defun my-go-mode-hook ()
-    (add-hook 'before-save-hook 'gofmt-before-save)
-    (add-to-list 'company-backends 'company-go)
-    (local-set-key (kbd "C-c C-f c") 'go-remove-unused-imports)
-    (if (not (string-match "go" compile-command))
-        (set (make-local-variable 'compile-command)
-             "go build -v && go test -v && go vet"))
-    (local-set-key (kbd "M-.") 'godef-jump)
-    (local-set-key (kbd "M-*") 'pop-tag-mark)
-    (setq go-guru-scope ".")
-    (go-guru-hl-identifier-mode)
-    ))
+  (add-to-list 'company-backends 'company-go)
+  (if (not (string-match "go" compile-command))
+	  (set (make-local-variable 'compile-command)
+		   "go build -v && go test -v && go vet"))
+  (go-guru-hl-identifier-mode))
