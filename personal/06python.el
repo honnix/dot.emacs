be680dba878c363136433bd58fb755119a3cb8a6
Hongxin Liang <honnix@spotify.com>
2020-02-11 14:11:52 +0100

    no warning for python indent

--- personal/06python.el
+++ personal/06python.el
@@ -2,5 +2,8 @@
   :ensure t
   :mode ("\\.py\\'" . python-mode)
   :bind ([remap python-shell-send-buffer] . comment-dwim)
+  :init
+  (setq python-indent-guess-indent-offset-verbose nil
+		python-indent-offset 4)
   :config
   (elpy-enable))
