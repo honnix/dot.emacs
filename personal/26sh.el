dd7a3a0308d91867184ec20b17b7e595e5907d6f
Hongxin Liang <honnix@spotify.com>
2020-04-07 22:10:21 +0200

    no more rebind

--- personal/26sh.el
+++ personal/26sh.el
@@ -9,7 +9,6 @@
   :mode (("\\.sh\\'" . sh-mode)
          ("\\.bash\\'" . sh-mode)
          ("\\.zshrc\\'" . sh-mode)
-         ("\\.zshenv\\'" . sh-mode))
-  :bind ([remap sh-case] . comment-dwim))
+         ("\\.zshenv\\'" . sh-mode)))
 
 ;;; 26sh.el ends here
