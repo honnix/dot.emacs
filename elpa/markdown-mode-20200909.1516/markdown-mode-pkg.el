367e67413016130e37335bcd1b8b2a8492b2dba0
Hongxin Liang <hxliang1982@gmail.com>
2020-09-27 20:25:36 +0200

    Upgrade

--- elpa/markdown-mode-20200909.1516/markdown-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /home/honnix/.emacs.d/elpa/markdown-mode-20200909.1516/markdown-mode.el  -*- no-byte-compile: t -*-
-(define-package "markdown-mode" "20200909.1516" "Major mode for Markdown-formatted text" '((emacs "25.1")) :commit "85b9720fcf2ed92a8ce7b968f11a4f54be230228" :keywords '("markdown" "github flavored markdown" "itex") :authors '(("Jason R. Blevins" . "jblevins@xbeta.org")) :maintainer '("Jason R. Blevins" . "jblevins@xbeta.org") :url "https://jblevins.org/projects/markdown-mode/")
