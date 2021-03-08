3d2a0967fa5307f5800e693209dfbf98deb50ca7
Hongxin Liang <honnix@spotify.com>
2021-03-08 12:37:17 +0100

    rubocop

--- personal/08ruby.el
+++ personal/08ruby.el
@@ -6,9 +6,19 @@
 
 (use-package ruby-mode
   :ensure nil
-  :hook ((ruby-mode . lsp-deferred)))
+  :hook ((ruby-mode . lsp-deferred)
+         (before-save . my-rb-before-save-hook))
+  :config
+  (defun my-rb-before-save-hook ()
+    (when (eq major-mode 'ruby-mode)
+      (lsp-format-buffer)
+      (lsp-organize-imports))))
 
 (use-package lsp-solargraph
   :ensure nil)
+
+(use-package rubocop
+  :ensure t
+  :hook (ruby-mode . rubocop-mode))
 
 ;;; 08ruby.el ends here
