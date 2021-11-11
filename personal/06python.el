64d41511678c5ba8a5f904656d7be5dbe654b3ec
Hongxin Liang <honnix@spotify.com>
2021-11-11 17:04:55 +0100

    set elpy-rpc-virtualenv-path to current

--- personal/06python.el
+++ personal/06python.el
@@ -15,7 +15,8 @@
         python-indent-offset 4)
   :config
   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules)
-        elpy-modules (delq 'elpy-module-highlight-indentation elpy-modules))
+        elpy-modules (delq 'elpy-module-highlight-indentation elpy-modules)
+        elpy-rpc-virtualenv-path 'current)
   (defun my-py-before-save-hook ()
     (when (eq major-mode 'python-mode)
       (lsp-format-buffer)
