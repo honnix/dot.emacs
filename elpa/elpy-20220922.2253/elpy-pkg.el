121083fc4b13df268dfe18eb702bfad47e13279e
Hongxin Liang <honnix@spotify.com>
2022-11-21 22:41:48 +0100

    Upgrade

--- elpa/elpy-20220922.2253/elpy-pkg.el
+++ elpa/elpy-20220922.2253/elpy-pkg.el
@@ -1,11 +1,11 @@
-(define-package "elpy" "20220322.41" "Emacs Python Development Environment"
+(define-package "elpy" "20220922.2253" "Emacs Python Development Environment"
   '((company "0.9.2")
     (emacs "24.4")
     (highlight-indentation "0.5.0")
     (pyvenv "1.3")
     (yasnippet "0.8.0")
     (s "1.11.0"))
-  :commit "1746e7009000b7635c0ea6f1559018143aa61642" :authors
+  :commit "ae7919d94659eb26d4146d4c3422c5f4c3610837" :authors
   '(("Jorgen Schaefer <contact@jorgenschaefer.de>, Gaby Launay" . "gaby.launay@protonmail.com"))
   :maintainer
   '("Jorgen Schaefer <contact@jorgenschaefer.de>, Gaby Launay" . "gaby.launay@protonmail.com")
