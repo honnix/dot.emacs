e8e9598e9695686ebcfcbd398465610ec7699c34
Hongxin Liang <honnix@spotify.com>
2023-12-22 14:30:33 +0100

    Upgrade

---
+++ elpa/hl-todo-20231214.1801/hl-todo-pkg.el
@@ -0,0 +1,15 @@
+(define-package "hl-todo" "20231214.1801" "Highlight TODO and similar keywords"
+  '((emacs "25.1")
+    (compat "29.1.4.2"))
+  :commit "86caf37992985fc9645762f7c622534b0547686d" :authors
+  '(("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainers
+  '(("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("convenience")
+  :url "https://github.com/tarsius/hl-todo")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
